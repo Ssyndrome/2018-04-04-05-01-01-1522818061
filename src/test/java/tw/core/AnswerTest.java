38ae35dd894db72259f08ae820f77ef785518670
915448715@qq.com <915448715@qq.com>
2018-04-20 16:13:16 +0800

    add answer&input test

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,52 @@
 package tw.core;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import tw.core.model.Record;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
+    Answer answer = new Answer();
+    Answer inputAns = new Answer();
+    Record record = new Record();
 
+    @Test
+    public void should_check_right_when_have_1_right_condition() {
+        answer = Answer.createAnswer("1 2 3 4");
+        inputAns = Answer.createAnswer("1 5 6 7");
+
+        record.increaseCurrentNum();
+        assertTrue(answer.check(inputAns).getValue()[0] == (record.getValue()[0]));
+        assertTrue(answer.check(inputAns).getValue()[1] == (record.getValue()[1]));
+    }
+
+    @Test
+    public void should_check_right_when_have_0_right_condition_but_4_right_numbers() {
+        answer = Answer.createAnswer("1 2 3 4");
+        inputAns = Answer.createAnswer("4 3 2 1");
+
+        record.increaseIncludeOnlyNum();
+        record.increaseIncludeOnlyNum();
+        record.increaseIncludeOnlyNum();
+        record.increaseIncludeOnlyNum();
+        assertTrue(answer.check(inputAns).getValue()[0] == (record.getValue()[0]));
+        assertTrue(answer.check(inputAns).getValue()[1] == (record.getValue()[1]));
+    }
+
+    @Test
+    public void should_check_right_when_have_2_right_condition_but_2_right_numbers() {
+        answer = Answer.createAnswer("1 2 3 4");
+        inputAns = Answer.createAnswer("1 5 6 4");
+
+        record.increaseCurrentNum();
+        record.increaseCurrentNum();
+        assertTrue(answer.check(inputAns).getValue()[0] == (record.getValue()[0]));
+        assertTrue(answer.check(inputAns).getValue()[1] == (record.getValue()[1]));
+    }
 }
--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,29 @@
 package tw.core;
 
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
+    InputValidator inputValidator = new InputValidator();
 
+    @Test
+    public void should_return_true_when_str_1_2_3_4() {
+        String str = "1 2 3 4";
+        assertTrue(inputValidator.validate(str));
+    }
+
+    @Test
+    public void should_return_false_when_no_space() {
+        String str = "1234";
+        assertFalse(inputValidator.validate(str));
+    }
 }
