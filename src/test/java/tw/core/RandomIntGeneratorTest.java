a3677d3000d900a204dccd77a2de69a569dff064
915448715@qq.com <915448715@qq.com>
2018-04-20 11:19:14 +0800

    add test for should throw error when ask fault

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,27 @@
 package tw.core;
 
 
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+import tw.core.generator.RandomIntGenerator;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
+    RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
 
+    @Test
+    public void should_throw_error_when_ask_fault() {
+        try {
+            randomIntGenerator.generateNums(1,2);
+        } catch (IllegalArgumentException e) {
+            assertTrue(e.getMessage().equals("Can't ask for more numbers than are available"));
+        }
+    }
 }
