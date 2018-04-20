47760ee7f6d19da4bdccb72cc7ede6ae7bed9556
915448715@qq.com <915448715@qq.com>
2018-04-20 11:48:40 +0800

    add test should generate right format'

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -14,7 +14,7 @@ import tw.core.generator.RandomIntGenerator;
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
-    RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+    private RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
 
     @Test
     public void should_throw_error_when_ask_fault() {
@@ -23,5 +23,11 @@ public class RandomIntGeneratorTest {
         } catch (IllegalArgumentException e) {
             assertTrue(e.getMessage().equals("Can't ask for more numbers than are available"));
         }
+    }
+
+    @Test
+    public void should_return_right_format() {
+        String numStr = randomIntGenerator.generateNums(9,4);
+        assertTrue(numStr.matches("^\\d \\d \\d \\d"));
     }
 }
