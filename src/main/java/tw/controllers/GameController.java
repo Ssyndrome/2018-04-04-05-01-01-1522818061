3a7006bda0edbee3a652b93349bd010eb805b869
915448715@qq.com <915448715@qq.com>
2018-04-22 14:03:30 +0800

    refractor GameController

--- src/main/java/tw/controllers/GameController.java
+++ src/main/java/tw/controllers/GameController.java
@@ -8,9 +8,7 @@ import tw.views.GameView;
 
 import java.io.IOException;
 
-/**
- * Created by jxzhong on 2017/5/19.
- */
+
 public class GameController {
     private final Game game;
     private final GameView gameView;
@@ -26,14 +24,12 @@ public class GameController {
     }
 
     public void play(InputCommand command) throws IOException {
-
-        if (game.checkCoutinue()) {
-            GuessResult guessResult = game.guess(command.input());
-            gameView.showGuessResult(guessResult);
+        if (!game.checkCoutinue()) {
+            gameView.showGameStatus(game.checkStatus());
+        } else {
+            gameView.showGuessResult(game.guess(command.input()));
             gameView.showGuessHistory(game.guessHistory());
             play(command);
-        } else {
-            gameView.showGameStatus(game.checkStatus());
         }
     }
 
