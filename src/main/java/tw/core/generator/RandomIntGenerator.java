c4e2d806a9ff50a286c1f134c5b4b7cd96d17767
915448715@qq.com <915448715@qq.com>
2018-04-22 14:13:51 +0800

    refractor RandomIntGenerator

--- src/main/java/tw/core/generator/RandomIntGenerator.java
+++ src/main/java/tw/core/generator/RandomIntGenerator.java
@@ -4,26 +4,23 @@ import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.Set;
 
-/**
- * Created by jxzhong on 2017/5/17.
- */
 public class RandomIntGenerator {
 
     public RandomIntGenerator() {
     }
 
     public String generateNums(Integer digitmax, Integer numbersOfNeed) {
-
         if (digitmax < numbersOfNeed) {
             throw new IllegalArgumentException("Can't ask for more numbers than are available");
         }
+        return String.join(" ", getStrings(digitmax, numbersOfNeed));
+    }
 
-        Random rng = new Random();
+    private Set<String> getStrings(Integer digitmax, Integer numbersOfNeed) {
         Set<String> generated = new LinkedHashSet<>();
         while (generated.size() < numbersOfNeed) {
-            Integer next = rng.nextInt(digitmax);
-            generated.add(next.toString());
+            generated.add(new Random().nextInt(digitmax)+"");
         }
-        return String.join(" ", generated);
+        return generated;
     }
 }
