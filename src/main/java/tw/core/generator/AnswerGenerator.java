8e0e2acea94f5ea9bf96db2323a4a244078e5409
915448715@qq.com <915448715@qq.com>
2018-04-22 14:05:11 +0800

    refractor AnswerGenerator

--- src/main/java/tw/core/generator/AnswerGenerator.java
+++ src/main/java/tw/core/generator/AnswerGenerator.java
@@ -4,9 +4,6 @@ import com.google.inject.Inject;
 import tw.core.Answer;
 import tw.core.exception.OutOfRangeAnswerException;
 
-/**
- * Created by jxzhong on 2017/5/17.
- */
 public class AnswerGenerator {
     private final RandomIntGenerator randomIntGenerator;
 
@@ -16,8 +13,7 @@ public class AnswerGenerator {
     }
 
     public Answer generate() throws OutOfRangeAnswerException {
-        String RandomNumStr = this.randomIntGenerator.generateNums(9, 4);
-        Answer answer = Answer.createAnswer(RandomNumStr);
+        Answer answer = Answer.createAnswer(this.randomIntGenerator.generateNums(9, 4));
         answer.validate();
         return answer;
     }
